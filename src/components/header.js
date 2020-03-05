b68da5d526b377d6550d11872f1cceb660f218b9
Nuno Machado <nunom4chado@gmail.com>
2020-03-05 14:14:23 +0000

    adds opacity on mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -84,11 +84,13 @@ const Nav = styled.nav`
     width: 100%;
     height: 100%;
     background: #121416;
+    opacity: 0;
     transform: scale(0);
     transition: .3s ease;
     z-index: 500;
 
     &.open {
+      opacity: 1;
       transform: scale(1);
 
       a {
